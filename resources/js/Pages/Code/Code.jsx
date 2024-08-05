ca30791e007a1f7991e8149624d28bbe0048f991
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-08-05 15:34:22 +0700

    Update Code.jsx

--- resources/js/Pages/Code/Code.jsx
+++ resources/js/Pages/Code/Code.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import Layout from '../../components/Layout'
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
 import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
@@ -8,26 +8,29 @@ import "prismjs/themes/prism.css";
 function Code() {
     const [code, setCode] = useState(
         "var message = 'Monaco Editor!' \nconsole.log(message);"
-      );
-      useEffect(()=>{
-      },[])
-  return (
-    <Layout>
-    <>
-    <Editor
-      value={code}
-      padding={10}
-      highlight={code => highlight(code, languages.js)}
-      onValueChange={(code) => setCode(code)}
-      style={{
-        fontFamily: "monospace",
-        fontSize: 17,
-        border: "1px solid black"
-      }}
-    />
-    </>
-    </Layout>
-  )
+    );
+    useEffect(() => {}, []);
+    return (
+        <Layout>
+            <>
+                <div className="row">
+                    <div className="col-md-3">
+                        <Editor
+                            value={code}
+                            padding={10}
+                            highlight={(code) => highlight(code, languages.js)}
+                            onValueChange={(code) => setCode(code)}
+                            style={{
+                                fontFamily: "monospace",
+                                fontSize: 17,
+                                border: "1px solid black",
+                            }}
+                        />
+                    </div>
+                </div>
+            </>
+        </Layout>
+    );
 }
 
-export default Code
+export default Code;
